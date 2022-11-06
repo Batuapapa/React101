86b10c9b1a4f8f4fecb0fdb09f01ef6bbf56aa18
Baturalp Alper HEPER <baturalpalperheper@gmail.com>
2022-11-06 22:58:56 +0300

    Conditional Render

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import {Title} from "./components/Title";
-import Header from "./components/Header";
+// import Header from './components/Header';
 import React from 'react';
-const name = "Baturalp Alper";
-let surname = "HEPER";
+import Login from './components/Login';
+
+const isLoggedIn = false;
+// const isLoggedIn = false;
+const fullname = "Baturalp Alper HEPER";
 function App() {
-  // return React.createElement("div",null,React.createElement("p",null));   --jsx olmasaydı kullanılacak yapı bu şekilde olacaktı
   return (
-    <React.Fragment>
-    {/* <div className="App">
-     <Title propsTitle='hello js'></Title>
-     <Title propsTitle='hello react'></Title> 
-     <Header/>
-    </div> */}
-    <div className='test'>
-      {name} {surname}
- 
-      <div>
-        <p>Test</p>
-      </div>
-
-        <label htmlFor='test'>Test</label>
-
-        <div>hello</div>
-     </div>
-    </React.Fragment> //kapsayıcı eleman gereklidir
+  <div className='test'>{
+     /* <div className="test">{isLoggedIn && <div>Hoşgeldin {fullname}</div>} 
+                        {!isLoggedIn && <a href='#'>Giriş yapın</a>}
+      </div> */}  
+    {
+      // isLoggedIn ? "Hoşgeldin " + fullname : "Giriş yapın"
+      // isLoggedIn ? "Hoşgeldin " + fullname : <a href="#">Giriş Yapın</a>
+      // isLoggedIn ? <div>Hoşgeldin {fullname}</div> : <a href="#">Giriş Yapın</a>
+      //  isLoggedIn ? <div>Hoşgeldin {fullname}</div> : null }
+      //  isLoggedIn ? <div>Hoşgeldin {fullname}</div> : <Header/>}
+       isLoggedIn ? <div>Hoşgeldin {fullname}</div> : <Login/>}
+  </div>
   );
 }  
-
 export default App;
