50e9f5bf1a357c82da66b4b75e0f1c77cd27a43c
Baturalp Alper HEPER <baturalpalperheper@gmail.com>
2022-11-06 20:44:39 +0300

    Rendering Variables in Components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import {Title} from "./components/Title";
 import Header from "./components/Header";
 import React from 'react';
+const name = "Baturalp Alper";
+let surname = "HEPER";
 function App() {
   // return React.createElement("div",null,React.createElement("p",null));   --jsx olmasaydı kullanılacak yapı bu şekilde olacaktı
   return (
     <React.Fragment>
-    <div className="App">
+    {/* <div className="App">
      <Title propsTitle='hello js'></Title>
      <Title propsTitle='hello react'></Title> 
      <Header/>
-    </div>
-    <>
-    <div className='test'>Test</div>
-    <label htmlFor='test'>Test</label>
-    </>
+    </div> */}
+    <div className='test'>
+      {name} {surname}
+ 
+      <div>
+        <p>Test</p>
+      </div>
+
+        <label htmlFor='test'>Test</label>
+
+        <div>hello</div>
+     </div>
     </React.Fragment> //kapsayıcı eleman gereklidir
   );
 }  
