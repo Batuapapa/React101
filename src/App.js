b7b7751a2103283e5c61e7533d5d0ebf44abde71
Baturalp Alper HEPER <baturalpalperheper@gmail.com>
2022-11-06 20:37:14 +0300

    Components and JSX Rules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {Title} from "./components/Title";
+import Header from "./components/Header";
+import React from 'react';
 function App() {
+  // return React.createElement("div",null,React.createElement("p",null));   --jsx olmasaydı kullanılacak yapı bu şekilde olacaktı
   return (
+    <React.Fragment>
     <div className="App">
      <Title propsTitle='hello js'></Title>
-     <Title propsTitle='hello react'></Title>
+     <Title propsTitle='hello react'></Title> 
+     <Header/>
     </div>
+    <>
+    <div className='test'>Test</div>
+    <label htmlFor='test'>Test</label>
+    </>
+    </React.Fragment> //kapsayıcı eleman gereklidir
   );
-}
+}  
 
 export default App;
