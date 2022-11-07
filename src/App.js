ed3b5219173477799cf26cf642f486d6ea275d3f
Baturalp Alper HEPER <baturalpalperheper@gmail.com>
2022-11-07 22:17:17 +0300

    Study with Object and Array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
-import React from 'react';
 import User from "./components/User";
+
+const user = {
+  name : "Baturalp Alper HEPER",
+  city : "Bursa",
+  age : 24,
+};
+
 function App() {
   return (
   <div> 
-    <User name="Baturalp Alper HEPER" city={"Bursa"} age={24}/>
-    <User name="Murat BOZ" city={"Bursa"} age={32}/>
-    <User name="Zeynep BASTIK" city={"Bursa"} age={27}/>
-
-  </div>
+    <User data = {user} friends ={["Ahmet","Murat","Ayşe","Fatma"]}/>
+   </div> 
   );
 }  
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function User(props) {
-    console.log(props); 
+  // function User({data:{name,age,city}}) {
+  // function User (props) {
+  // function User (data) {
+    function User ({data, friends}) {
+    console.log(friends);
+    // console.log(props); 
+
+    // const {name, age, city} =props.data;
+    // const {data} = props;
+    // const data = props.data;
   return (
     <div>
-    User
-    <div>İsim : {props.name} </div>
-    <div>Yaş : {props.age} </div>
-    <div>Şehir :  {props.city} </div>
+    Kişi
+    <div>İsim : {data.name} </div>
+    <div>Yaş : {data.age} </div>
+    <div>Şehir :  {data.city} </div>
+    <h3>Arkadaşları</h3>
+    {
+      friends.map((friend, i) =>
+    (<div key={i}>{friend}</div>)
+    
+      // { return <div key={i}>{friend}</div>;}
+
+    // (
+    // <div key={i}>{friend}</div>
+    // )
+    )}
     <hr/>
     </div>
-  )
+  );
 }
 
 export default User;
