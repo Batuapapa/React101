1f1e1cb1d03b25b66247ce41a33a579470931279
Baturalp Alper HEPER <baturalpalperheper@gmail.com>
2022-11-09 23:54:33 +0300

    State for Form Elements 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,40 @@
 
 import React, {useState} from 'react';
 function Form () {
-    const [name,setName] = useState("");
-    const [surname,setSurname] = useState("");
-    const [gender,setGender] = useState("0");
+    const [form, setForm] = useState({name: "", surname: "", gender: "0"});
+    const handleChange =(e) => {
+        setForm({...form,[e.target.name]: e.target.value});
+        // setForm((prev) => ({...form,[e.target.name]: e.target.value}));
+
+
+    };
+    // const [name,setName] = useState("");
+    // const [surname,setSurname] = useState("");
+    // const [gender,setGender] = useState("0");
 
   return (
     <div>
         <div>İSİM - SOYİSİM</div>
         <input placeholder='İsim' 
-         value={name} 
-         onChange={(event) => setName(event.target.value)}/>     
+         name='name'
+         value={form.name} 
+         onChange={handleChange}/>     
          <input placeholder='Soyisim'
-         value={surname}
-         onChange={(event) => setSurname(event.target.value)}
+         name='surname'
+         value={form.surname}
+         onChange={handleChange}
          /> 
         <br/><br/>
         <div>
             <div>Cinsiyet</div>
-            <select value={gender} onChange={(event) => setGender(event.target.value)}>
+            <select value={form.gender} onChange={handleChange} name="gender">
                 <option value={0}>Erkek</option>
                 <option value={1}>Kadın</option>
-            </select>
+             </select>
         </div>
         <hr/>
-        <div>İsim : <strong>{name} {surname}</strong></div>
-        <div>Cinsiyet : <strong>{gender === '0' ? 'Erkek': 'Kadın'}</strong></div>
+        <div>İsim : <strong>{form.name} {form.surname}</strong></div>
+        <div>Cinsiyet : <strong>{form.gender === '0' ? 'Erkek': 'Kadın'}</strong></div>
     </div>
   )
 }
